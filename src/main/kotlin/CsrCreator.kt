a56ce1a41ccdd9623efaa242545b78402ea595e1
Joffrey B <joffreybonifay83@gmail.com>
2022-10-19 16:37:32 +0200

    add parser impl

--- src/main/kotlin/CsrCreator.kt
+++ src/main/kotlin/CsrCreator.kt
@@ -3,24 +3,47 @@ import java.io.OutputStream // ktlint-disable filename
 
 fun OutputStream.writeCsv(channels: List<Channel>) {
     val writer = bufferedWriter()
-    val sb = StringBuilder()
 
-    writeHeader(channels, sb, writer)
+    writeHeader(channels, writer)
+
+    writeData(channels, writer)
 
     writer.flush()
 }
 
-private fun writeHeader(
-    channels: List<Channel>,
-    sb: StringBuilder,
-    writer: BufferedWriter
-) {
+private fun writeHeader(channels: List<Channel>, writer: BufferedWriter) {
+    val sb = StringBuilder()
     sb.append("Sample,")
-    channels.forEach {
+    channels.forEachIndexed { index, it ->
         sb.append(it.name)
-        sb.append(",")
+        if (index != channels.size - 1) {
+            sb.append(",")
+        }
     }
-    sb.deleteAt(sb.length - 1)
+    writer.write(sb.toString())
+    writer.newLine()
+}
+
+private fun writeData(channels: List<Channel>, writer: BufferedWriter) {
+    val sb = StringBuilder()
+    val maxDataCount = channels.maxBy { it.data_count }.data_count
+
+    for (i in 0 until maxDataCount) {
+        sb.append(i + 1)
+        sb.append(",")
+
+        for ((index, chann) in channels.withIndex()) {
+            sb.append(chann.data.getOrNull(i) ?: "")
+            if (index != channels.size - 1) {
+                sb.append(",")
+            }
+        }
+
+        if (i != maxDataCount - 1) {
+            sb.append("\n")
+        }
+    }
+
     writer.write(sb.toString())
     writer.newLine()
 }
--- src/test/kotlin/CsrCreatorTest.kt
+++ src/test/kotlin/CsrCreatorTest.kt
@@ -6,26 +6,94 @@ import java.io.ByteArrayOutputStream
 class CsrCreatorTest {
 
     @Test
-    fun write_csv() {
+    fun shouldInsertHeaderWithChannelNames() {
         val channels = listOf(
             createFakeChannel("Brake"),
             createFakeChannel("AirTemp"),
             createFakeChannel("AirDensity")
         )
-        val csv = ByteArrayOutputStream().apply { writeCsv(channels) }.toByteArray().let { String(it) }
+        val csvResult = ByteArrayOutputStream().apply { writeCsv(channels) }.toByteArray().let { String(it) }
 
         assertEquals(
-            "Sample,Brake,AirTemp,AirDensity\n",
-            csv
+            "Sample,Brake,AirTemp,AirDensity\n\n",
+            csvResult
         )
     }
 
-    private fun createFakeChannel(channelName: String): Channel {
+    @Test
+    fun shouldInsertValueForOneChannel() {
+        val channels = listOf(
+            createFakeChannel("Brake", 10)
+        )
+
+        val csvResult = ByteArrayOutputStream().apply { writeCsv(channels) }.toByteArray().let { String(it) }
+
+        assertEquals(
+            "Sample,Brake\n1,10\n",
+            csvResult
+        )
+    }
+
+    @Test
+    fun shouldInsertOneValueForTwoChannels() {
+        val channels = listOf(
+            createFakeChannel("Brake", 10),
+            createFakeChannel("Gaz", 20)
+        )
+
+        val csvResult = ByteArrayOutputStream().apply { writeCsv(channels) }.toByteArray().let { String(it) }
+
+        assertEquals(
+            "Sample,Brake,Gaz\n1,10,20\n",
+            csvResult
+        )
+    }
+
+    @Test
+    fun shouldInsertThreeValueForTwoChannels() {
+        val channels = listOf(
+            createFakeChannel("Brake", 10, 20, 30),
+            createFakeChannel("Gaz", 40, 50, 60)
+        )
+
+        val csvResult = ByteArrayOutputStream().apply { writeCsv(channels) }.toByteArray().let { String(it) }
+
+        assertEquals(
+            """Sample,Brake,Gaz
+                |1,10,40
+                |2,20,50
+                |3,30,60
+                |
+            """.trimMargin(),
+            csvResult
+        )
+    }
+
+    @Test
+    fun shouldInsertValueForChannelsWithDataOfDifferentSize() {
+        val channels = listOf(
+            createFakeChannel("Brake", 10),
+            createFakeChannel("Gaz", 40, 50, 60)
+        )
+
+        val csvResult = ByteArrayOutputStream().apply { writeCsv(channels) }.toByteArray().let { String(it) }
+        assertEquals(
+            """Sample,Brake,Gaz
+                |1,10,40
+                |2,,50
+                |3,,60
+                |
+            """.trimMargin(),
+            csvResult
+        )
+    }
+
+    private fun createFakeChannel(channelName: String, vararg value: Int): Channel {
         return Channel(
             0,
             0,
             0,
-            0,
+            value.size,
             0,
             0,
             0,
@@ -39,6 +107,10 @@ class CsrCreatorTest {
             "",
             "",
             ByteArray(0)
-        )
+        ).apply {
+            for (v in value) {
+                this.data.add(v)
+            }
+        }
     }
 }
